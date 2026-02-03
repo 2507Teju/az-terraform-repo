2dcc25ed90a216845e16de82aa79bb247274a2da
2507Teju <tgowdatejashwini@gmail.com>
2026-02-03 11:08:05 +0530

    creating new rg and storage account

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "sec-rg"{
+resource "azurerm_resource_group" "third-rg"{
     name = var.rg_name
     location = var.location
 }
 
-resource "azurerm_storage_account" "firststorage"{
+resource "azurerm_storage_account" "secondstorage"{
     name = var.storage
     resource_group_name = resource.azurerm_resource_group.sec-rg.name
     location = resource.azurerm_resource_group.sec-rg.location
